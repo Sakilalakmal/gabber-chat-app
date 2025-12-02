beaa07a2e10ad4e7394fd521cfb52c18668d4d97
Ganwari Nakathige Sakila Lakmal Nandasiri CB014383 <164737611+Sakilalakmal@users.noreply.github.com>
2025-12-02 19:29:44 +0530

    Implement user authentication with signup, login, and logout functionality

--- backend/src/controllers/auth-controllers.js
+++ backend/src/controllers/auth-controllers.js
@@ -1,5 +1,118 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
 export const authControllers = {
-  login: (req,res) => {
-    
+  SignUp: async (req, res) => {
+    const { email, password, fullName } = req.body;
+
+    try {
+      if (!email || !password || !fullName) {
+        return res.status(400).json({ message: "All fields are required" });
+      }
+
+      if (password.length < 6) {
+        return res
+          .status(400)
+          .json({ message: "Password must be at least 6 characters long" });
+      }
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+      if (!emailRegex.test(email)) {
+        return res
+          .status(400)
+          .json({ message: "Invalid email format please enter valid email" });
+      }
+
+      const existingUser = await User.findOne({ email });
+
+      if (existingUser) {
+        return res.status(400).json({
+          message: "Account already exists with this email try to log in",
+        });
+      }
+
+      const index = Math.floor(Math.random() * 100) + 1;
+      const randomAvatar = `https://avatar.iran.liara.run/public/${index}.png`;
+
+      const newuser = await User.create({
+        email,
+        fullName,
+        password,
+        profilePic: randomAvatar,
+      });
+
+      await newuser.save();
+
+      // TODO : create a user in stream too [use $transactions]
+
+      const token = jwt.sign({ userId: newuser._id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+
+      res.cookie("JWT", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+      });
+
+      res.status(201).json({
+        message: "User registered successfully",
+        user: newuser,
+        token,
+      });
+    } catch (error) {
+      console.error("Error during user signup:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  },
+
+  Login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+      }
+
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ message: "user not found please sign up first" });
+      }
+
+      const isCorrectPassword = await user.comparePassword(password);
+
+      if (!isCorrectPassword) {
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
+
+      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+
+      res.cookie("JWT", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+      });
+
+      res.status(200).json({
+        message: "Login successful",
+        user,
+        token,
+      });
+    } catch (error) {
+      console.error("Error during user login:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  },
+
+  LogOut: async (req, res) => {
+    res.clearCookie("JWT");
+    res.status(200).json({ message: "Logout successful" });
   },
 };
--- backend/src/routes/auth-routes.js
+++ backend/src/routes/auth-routes.js
@@ -1,5 +1,12 @@
 import express from "express";
+import { authControllers } from "../controllers/auth-controllers.js";
 
 const authRouter = express.Router();
+
+authRouter.post("/signup", authControllers.SignUp);
+
+authRouter.post("/login", authControllers.Login);
+
+authRouter.post("/logout", authControllers.LogOut);
 
 export default authRouter;
